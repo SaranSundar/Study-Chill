22a39e0d18115be019e8870841e7f7529a3fee0e
Saran Sundararajan <saran@wireless-10-147-122-179.public.utexas.edu>
2018-10-21 01:43:57 -0500

    GPS

--- app/src/main/java/com/sszg/studygroups/fragments/NewStudyRoom.java
+++ app/src/main/java/com/sszg/studygroups/fragments/NewStudyRoom.java
@@ -1,7 +1,10 @@
 package com.sszg.studygroups.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,20 +13,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.sszg.studygroups.R;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.app.Activity.RESULT_OK;
 
 
 public class NewStudyRoom extends Fragment {
     private TextView locationText;
-    private Button locationButton;
+    private ImageView profileURL;
+    private EditText professorName, courseName, roomNumber, time;
+    private ImageButton locationButton;
     private FusedLocationProviderClient client;
 
     @Nullable
@@ -32,6 +42,11 @@ public class NewStudyRoom extends Fragment {
         View view = inflater.inflate(R.layout.new_study_room, null);
         locationButton = view.findViewById(R.id.location_button);
         locationText = view.findViewById(R.id.location_text);
+        professorName = view.findViewById(R.id.professor_name);
+        courseName = view.findViewById(R.id.course_name);
+        roomNumber = view.findViewById(R.id.room_number);
+        time = view.findViewById(R.id.time);
+        profileURL = view.findViewById(R.id.profile_url);
         return view;
     }
 
@@ -44,6 +59,7 @@ public class NewStudyRoom extends Fragment {
             @Override
             public void onClick(View v) {
                 if (ActivityCompat.checkSelfPermission(getActivity(), ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getActivity(), "GPS Permissions need to be enabled", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 client.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
@@ -51,11 +67,36 @@ public class NewStudyRoom extends Fragment {
                     public void onSuccess(Location location) {
                         if (location != null) {
                             locationText.setText(String.valueOf(" Lat: " + location.getLatitude() + " Long: " + location.getLongitude()));
+                        } else {
+                            Toast.makeText(getActivity(), "Not able to get GPS Coordinates", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
             }
         });
+        final Context context = getContext();
+        final Fragment thisFragment = this;
+        profileURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.activity().start(context, thisFragment);
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                profileURL.setImageURI(resultUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                System.out.println(error.toString());
+            }
+        }
     }
 
     private void requestPermission() {
